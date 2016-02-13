4d8758d28d84f2bb97276d7464110e00669a3c0b
Zachary Blank <zach@hungryinc.com>
2016-02-12 20:20:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = function() {
+    return function(path, env) {
+        switch (env) {
+            case 'merchant' :
+                return process.env.merchant.protocol + process.env.merchant.host + path
+            break;
+            case 'assets' :
+                return process.env.assets.protocol + process.env.assets.host + path
+            break;
+            case 'api' :
+                return process.env.api.protocol + process.env.api.host + path
+            break;
+            case 'consumer' :
+                return process.env.consumer.protocol + process.env.consumer.host + path
+            break;
+        }
+    }
+}
